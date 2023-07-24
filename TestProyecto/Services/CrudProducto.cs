d9f5152a26fb6441e233c6413d3a4c876a846c8a
ManuelDeJesusMex <zyntus99@gmail.com>
2023-07-24 14:14:30 -0500

    CrudProducto F1

--- TestProyecto/Services/CrudProducto.cs
+++ TestProyecto/Services/CrudProducto.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestProyecto.Entities;
 
 namespace TestProyecto.Services
 {
     public class CrudProducto
     {
+        public void CreateProducto (Producto request)
+        {
+			try
+			{
+				if (request != null)
+				{
+					
+				}
+			}
+			catch (Exception ex)
+			{
+
+				throw new Exception ("Error: "+ex.Message);
+			}
+        }
+
     }
 }
